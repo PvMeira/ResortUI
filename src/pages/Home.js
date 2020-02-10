4df02a9c9c554d852e581daa7ed593bdd560a201
Pedro Vitor Meira <pedro.ciria@totvspartners.com.br>
2020-02-09 22:10:20 -0300

    Creating Services and Title Components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
 import Banner from "../components/banner/Banner";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Services from "../components/services-component/Services";
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="Luxurious Rooms" subTitle="Deluxe Rooms starting at $99">
-        <Link to="/rooms" className="btn-primary">Our Rooms</Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title="Luxurious Rooms" subTitle="Deluxe Rooms starting at $99">
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
